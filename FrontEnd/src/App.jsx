c5112b32066b04791d21cc846b898351182bd931
kunalsharma108 <kunalsharma0422@gmail.com>
2024-03-30 19:23:26 +0530

    again some updates

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
   const [data, setData] = useState([{}]);
   const [backEndonline, setBackendOnline] = useState(false)
-  
+
   const fetchData = async () => {
     try {
       const response = await axios.get('http://localhost:5000/isOnline');
       console.log(response.data)
       setBackendOnline(true)
-      
+
     } catch (error) {
       console.log(`There was an error : ${error}`)
     }
@@ -22,7 +22,11 @@ function App() {
 
   return (
     <>
-      
+      {
+        backEndonline == false ?
+          (<h1> backend is offline , my bad lil bro </h1>) :
+          (<h1>Backend is online , enjoy lil bro </h1>)
+      }
     </>
   )
 }
