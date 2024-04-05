292b817e87e06973e0f24668d89ca61a98afcc82
kunalsharma108 <kunalsharma0422@gmail.com>
2024-04-05 17:17:46 +0530

    added many things

--- FrontEnd/src/templates/Assets/Home/homePicturetop.jsx
+++ FrontEnd/src/templates/Assets/Home/homePicturetop.jsx
@@ -6,16 +6,16 @@ function homePicturetop(props) {
   return (
     <div className="home-big-pic">
         <div className="home-big-pic-content">
-            <div className="logo">
+            <div className="logo ">
                 <img src="src\templates\Assets\images\dollor-icon.png" alt="" width={'200px'} height={'200px'}/>
             </div>
             <div className="home-heading titillium-web-heavy">Expense Manager</div>
-            <div className="home-sub-heading titillium-web-light">Take Control of Your Finances with Ease</div>
+            <div className="home-sub-heading titillium-web-light">-Take Control of Your Finances with Ease-</div>
             <div className="button-row ">
                 {props.cookie == false ? (
-                    <Link className="button text-light mx-3 titillium-web-heavy" to={'/user/SignUp'}>Sign Up</Link>
+                    <Link className="button text-light mx-3 titillium-web-heavy" to={'/user/SignUp'}>Sign Up/Sign In</Link>
                 ) : null}
-                <Link className="button text-light mx-3 titillium-web-heavy " to={'/user-guide'}>How to ?</Link>
+                <Link className="button text-light mx-3 titillium-web-heavy " to={'/user-guide'}>How to Manage ?</Link>
             </div>
         </div>
     </div>
