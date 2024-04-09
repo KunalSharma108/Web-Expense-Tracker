e3cb81078cdf06a72ac3f089f6dd051fcb0c3169
kunalsharma108 <kunalsharma0422@gmail.com>
2024-04-09 15:16:14 +0530

    added more things on home page

--- FrontEnd/src/templates/Assets/Home/Home.jsx
+++ FrontEnd/src/templates/Assets/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import HomePicturetop from './homePicturetop'
 import About from './About'
+import WhyWET from './WhyWET'
 
 function Home(props) {
   return (
     <div className='home-main'>
       <HomePicturetop cookie = {props.cookie}/>
       <About />
+      <WhyWET />
     </div>
   )
 }
