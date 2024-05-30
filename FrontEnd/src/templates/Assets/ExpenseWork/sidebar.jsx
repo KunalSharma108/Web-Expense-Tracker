438e2275d94c8f072db75f8bad34c592f48d170f
kunalsharma108 <kunalsharma0422@gmail.com>
2024-05-30 17:05:57 +0530

    rough day

--- FrontEnd/src/templates/Assets/ExpenseWork/sidebar.jsx
+++ FrontEnd/src/templates/Assets/ExpenseWork/sidebar.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useRef, useState, useEffect } from 'react';
 
-function sidebar(props) {
+function Sidebar(props) {
+  const [showDialog, setShowDialog] = useState(false);
+  const [trackerName, setTrackerName] = useState('');
+
   return (
-    <div >
-        <div className="sidebar">
-            asfdfdsa
+    <div>
+      <div className="sidebar">
+        <div className="top-btn" onClick={handleOpenDialog}>
+          <div className="top-btn-content">New Tracker</div>
         </div>
+      </div>
+
+      <div className={`custom-dialog ${showDialog == false ? 'hidden' : null}`}>
+        <div className="dialog-content">
+          <h2>Enter Tracker Name</h2>
+          <input
+            type="text"
+            value={trackerName}
+            onChange={(e) => setTrackerName(e.target.value)}
+            className="tracker-input"
+          />
+          <div className="btn-container">
+            <button onClick={handleSave} className="save-btn">Save</button>
+            <button onClick={handleCloseDialog} className="cancel-btn">Cancel</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-
-export default sidebar
-
+export default Sidebar;
