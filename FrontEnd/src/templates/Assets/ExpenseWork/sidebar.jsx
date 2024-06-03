bd0d4f59e590fa0f6a7b89e63fea5d67fd3cc031
kunalsharma108 <kunalsharma0422@gmail.com>
2024-06-03 16:08:14 +0530

    more progress

--- FrontEnd/src/templates/Assets/ExpenseWork/sidebar.jsx
+++ FrontEnd/src/templates/Assets/ExpenseWork/sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function Sidebar(props) {
   const [showDialog, setShowDialog] = useState(false);
@@ -6,39 +6,41 @@ function Sidebar(props) {
 
   const handleOpenDialog = () => {
     setShowDialog(true);
-  }
+  };
 
   const handleSave = () => {
-    console.log(`Tracker Name : ${trackerName}`)
-  }
+    console.log(`Tracker Name : ${trackerName}`);
+    handleCloseDialog();
+  };
 
   const handleCloseDialog = () => {
-    setShowDialog(false)
-  }
+    setShowDialog(false);
+  };
 
   return (
     <div>
-      <div className="sidebar" id='sidebar'>
+      <div className="sidebar" id="sidebar">
         <div className="top-btn" onClick={handleOpenDialog}>
           <div className="top-btn-content">New Tracker</div>
         </div>
       </div>
 
-      <div className={`custom-dialog ${showDialog == false ? 'hidden' : null}`}>
-        <div className="dialog-content">
-          <h2>Enter Tracker Name</h2>
-          <input
-            type="text"
-            value={trackerName}
-            onChange={(e) => setTrackerName(e.target.value)}
-            className="tracker-input"
-          />
-          <div className="btn-container">
-            <button onClick={handleSave} className="save-btn">Save</button>
-            <button onClick={handleCloseDialog} className="cancel-btn">Cancel</button>
+          <div className={`custom-dialog ${showDialog ? 'show' : 'hide'}`}>
+            <div className="dialog-content">
+              <h2>Tracker Name</h2>
+              <input
+                type="text"
+                value={trackerName}
+                onChange={(e) => setTrackerName(e.target.value)}
+                className="tracker-input"
+              />
+              <div className="btn-container">
+                <button onClick={handleSave} className="save-btn">Save</button>
+                <button onClick={handleCloseDialog} className="cancel-btn">Cancel</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+          
     </div>
   );
 }
